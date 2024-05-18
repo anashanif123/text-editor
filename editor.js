34e2c6c83a39cb3ccdfc0db3c849532802f82e45
anas <opmode56@gmail.com>
2024-05-19 00:32:42 +0500

    text edior

---
+++ editor.js
@@ -0,0 +1,44 @@
+
+
+document.getElementById('bold').addEventListener('click', function() {
+    var inputText = document.getElementById('inputs');
+    inputText.style.fontWeight = (inputText.style.fontWeight === 'bold'? 'normal' : 'bold');
+  });
+  
+  document.getElementById('italic').addEventListener('click', function()  {
+    var inputText = document.getElementById('inputs');
+    inputText.style.fontStyle = (inputText.style.fontStyle === 'italic'? 'normal' : 'italic');
+  });
+  
+  document.getElementById('underline').addEventListener('click', function()  {
+    var inputText = document.getElementById('inputs');
+    inputText.style.textDecoration = (inputText.style.textDecoration === 'underline'? 'none' : 'underline');
+  });
+  
+  document.getElementById('fontsize').addEventListener('click', function()  {
+    var inputText = document.getElementById('inputs');
+    var fontSize = document.getElementById('sizes').value;
+    inputText.style.fontSize = fontSize;
+  });
+  
+  document.getElementById('fontfamily').addEventListener('click',function ()  {
+    var inputText = document.getElementById('inputs');
+    var fontFamily = document.getElementById('fontfamilies').value;
+    inputText.style.fontFamily = fontFamily;
+  });
+  
+  document.getElementById('textcolor').addEventListener('click',function ()  {
+    var inputText = document.getElementById('inputs');
+    var colorPicker = document.getElementById('textcolor').children[0];
+    inputText.style.color = colorPicker.value;
+});
+document.getElementById('clear').addEventListener('click', function() {
+  var inputText = document.getElementById('inputs');
+  inputText.value = ''; 
+  inputText.style.fontWeight = 'normal';
+  inputText.style.fontStyle = 'normal';
+  inputText.style.textDecoration = 'none';
+  inputText.style.fontSize = ''; 
+  inputText.style.fontFamily = ''; 
+  inputText.style.color = ''; 
+});
