cb620972e9337ea5294cc000c24b572e04cda8cb
anas <opmode56@gmail.com>
2024-05-20 16:38:10 +0500

    editor

--- editor.js
+++ editor.js
@@ -1,28 +1,28 @@
 
+var inputText = document.getElementById('inputs');
 
 document.getElementById('bold').addEventListener('click', function() {
-    var inputText = document.getElementById('inputs');
+    // var inputText = document.getElementById('inputs');
     inputText.style.fontWeight = (inputText.style.fontWeight === 'bold'? 'normal' : 'bold');
   });
   
   document.getElementById('italic').addEventListener('click', function()  {
-    var inputText = document.getElementById('inputs');
+    // var inputText = document.getElementById('inputs');
     inputText.style.fontStyle = (inputText.style.fontStyle === 'italic'? 'normal' : 'italic');
   });
   
   document.getElementById('underline').addEventListener('click', function()  {
-    var inputText = document.getElementById('inputs');
     inputText.style.textDecoration = (inputText.style.textDecoration === 'underline'? 'none' : 'underline');
   });
   
   document.getElementById('fontsize').addEventListener('click', function()  {
-    var inputText = document.getElementById('inputs');
+    // var inputText = document.getElementById('inputs');
     var fontSize = document.getElementById('sizes').value;
     inputText.style.fontSize = fontSize;
   });
   
   document.getElementById('fontfamily').addEventListener('click',function ()  {
-    var inputText = document.getElementById('inputs');
+    // var inputText = document.getElementById('inputs');
     var fontFamily = document.getElementById('fontfamilies').value;
     inputText.style.fontFamily = fontFamily;
   });
